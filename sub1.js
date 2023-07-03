b3b3356f30dd6674a5fa46b10c17195d89bf8d49
Lakshmi <Rajyalakshmi.gudimetla@ge.com>
2023-07-03 15:20:52 +0530

    Changespushed-Telenor Test

--- sub1.js
+++ sub1.js
@@ -5,16 +5,16 @@ const password = '14d1b530';
 const clientId = 'f44a7c6a-219f-417d-9a1d-f2bafd38ad53';
 const host = '10.246.0.10:1883';
 //const MQTT_TOPIC = 'MC/V1/testing';
-const client = mqtt.connect('mqtt://test.mosquitto.org:1883');
-const client1 = mqtt.connect(`mqtt://${username}:${password}@${host}`, {
+//const client = mqtt.connect('mqtt://test.mosquitto.org:1883');
+const client = mqtt.connect(`mqtt://${username}:${password}@${host}`, {
     clientId,
     clean: true,
     rejectUnauthorized: false
 });
-//const topic='MC/V1/testing';
-const topic = 'testing_new';
+const topic = 'MC/V1/testing';
+//const topic = 'testing_new';
 client.on('connect', () => {
-    client.subscribe('testing_new');
+    client.subscribe('MC/V1/testing');
 });
 
 client.on('message', (topic, message1) => {
