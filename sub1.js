820ab8de9b8f1022aa4915a72e0caf27a8c13665
Lakshmi <Rajyalakshmi.gudimetla@ge.com>
2023-07-05 17:15:35 +0530

    changes pushed 7/5

--- sub1.js
+++ sub1.js
@@ -5,7 +5,7 @@ const password = '14d1b530';
 const clientId = 'f44a7c6a-219f-417d-9a1d-f2bafd38ad53';
 const host = '10.246.0.10:1883';
 //const MQTT_TOPIC = 'MC/V1/testing';
-//const client = mqtt.connect('mqtt://test.mosquitto.org:1883');
+const client1 = mqtt.connect('mqtt://test.mosquitto.org:1883');
 const client = mqtt.connect(`mqtt://${username}:${password}@${host}`, {
     clientId,
     clean: true,
@@ -20,4 +20,5 @@ client.on('connect', () => {
 client.on('message', (topic, message1) => {
     console.log(`Received message on topic ${topic}: ${message1}`);
     // Process the received message as needed
+
 });
